31bd0552b92459159190aea1d804a9727e29a363
Uhupi <santino.lange@gmail.com>
2026-05-01 19:48:18 +0200

    fix: remove PHP file exclusion logic from build process

--- vite.config.js
+++ vite.config.js
@@ -10,33 +10,6 @@ export default defineConfig({
       name: 'exclude-php-files',
       apply: 'build',
       enforce: 'post',
-      generateBundle(_options, bundle) {
-        // Remove PHP files from the bundle
-        Object.keys(bundle).forEach(fileName => {
-          if (fileName.endsWith('.php')) {
-            delete bundle[fileName]
-          }
-        })
-      },
-      writeBundle() {
-        // Remove any .php files that were copied to dist
-        const distDir = 'dist'
-        if (fs.existsSync(distDir)) {
-          const removePhpFiles = (dir) => {
-            const files = fs.readdirSync(dir)
-            files.forEach(file => {
-              const filePath = path.join(dir, file)
-              const stat = fs.statSync(filePath)
-              if (stat.isDirectory()) {
-                removePhpFiles(filePath)
-              } else if (file.endsWith('.php')) {
-                fs.unlinkSync(filePath)
-              }
-            })
-          }
-          removePhpFiles(distDir)
-        }
-      }
     }
   ],
   server: {
